56fb3c0995e92e7dd499407c91691d10bf01da00
Maycon J Pereira <mayconjovan@gmail.com>
2020-03-25 22:52:29 -0300

    Conectando ao MongoDB com repository e service

---
+++ src/main/java/com/mjp/workshopmongo/repository/UserRepository.java
@@ -0,0 +1,13 @@
+package com.mjp.workshopmongo.repository;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import com.mjp.workshopmongo.domain.User;
+
+@Repository
+public interface UserRepository extends MongoRepository<User, String> {
+	
+
+	
+}
