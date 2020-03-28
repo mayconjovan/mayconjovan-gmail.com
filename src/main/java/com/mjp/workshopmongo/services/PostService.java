29c3a9269c7701d265ddef2dd930489169415d3d
Maycon J Pereira <mayconjovan@gmail.com>
2020-03-28 00:18:49 -0300

    Obtendo post por Id

---
+++ src/main/java/com/mjp/workshopmongo/services/PostService.java
@@ -0,0 +1,25 @@
+package com.mjp.workshopmongo.services;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mjp.workshopmongo.domain.Post;
+import com.mjp.workshopmongo.repository.PostRepository;
+import com.mjp.workshopmongo.services.exception.ObjectNotFoundException;
+
+@Service
+public class PostService {
+	
+	@Autowired
+	private PostRepository repo;	
+	
+	public Post findById(String id) {
+		Optional<Post> obj = repo.findById(id);
+		return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	
+	
+}
